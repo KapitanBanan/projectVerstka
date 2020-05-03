766928510c9dd98844b2436593f7f88ec86ba7cf
Maksim Mikheev <maksim.mikheev@foodpanda.com>
2020-05-03 16:53:41 +0800

    working navigation

--- menu.js
+++ menu.js
@@ -1,7 +1,23 @@
+const offsetNavbarTransparent = 50;
+
 window.onscroll = function() {
-    if (window.pageYOffset > 50) {
+    if (window.pageYOffset > offsetNavbarTransparent) {
       document.getElementById("navbar").classList.add('solid');
     } else {
       document.getElementById("navbar").classList.remove('solid');
     }
   }
+
+document.querySelector('.menu__burger').addEventListener('click', function(e) {
+  e.currentTarget.parentElement.classList.toggle('opened');
+  const isOpening = e.currentTarget.parentElement.classList.contains('opened');
+  var navbar = document.getElementById("navbar");
+
+  if (isOpening) {
+    navbar.classList.add('solid');
+  }
+
+  if (!isOpening && window.pageYOffset < offsetNavbarTransparent) {
+    navbar.classList.remove('solid');
+  }
+})
